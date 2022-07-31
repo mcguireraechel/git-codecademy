6aa06f34b067cc1c70495815e61d6b916fc2a65c
mcguireraechel <79418928+mcguireraechel@users.noreply.github.com>
2022-07-30 17:08:43 -0700

    Added variable x

---
+++ src/delete/Solution.java
@@ -0,0 +1,31 @@
+package delete;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+	
+	public int reverse(int x) {
+		
+		
+		
+		
+		
+		return 0;
+	}
+        
+    
+
+	public static void main(String[] args) {
+		
+		
+		  int num = 123;
+		  
+		  Integer number = num;
+		  int x = 648;
+		  System.out.println(Integer.reverse(number.intValue()));
+		
+	
+	}
+
+}
